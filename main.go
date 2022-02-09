d4b2d0e1f24bb0e792d2bf6ea8ceaa6b92e5a570
Vishnu prasad <vishnuindira123@gmail.com>
2022-02-09 15:01:39 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,32 +1,116 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/gorilla/mux"
 )
 
-func main() {
+// defined type Employee
+type Employee struct {
+	Id   string `json:"Id"`
+	Name string `json:"name"`
+}
 
-	e := echo.New()
+// declared a global array
+var Employees []Employee
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+// to get root page (/)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Root page")
+	fmt.Println("Root page")
+}
 
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
+// to return all employee records (/emplpoyees)
+func returnAllEmployees(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println("Endpoint Hit: returnAllEmployees")
+	json.NewEncoder(w).Encode(Employees)
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
+}
 
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8080"
+// to return single employee records (/emplpoyees/{empid})
+func returnSingleEmp(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Fprintf(w, "Key: "+key)
+
+	for _, emp := range Employees {
+		if emp.Id == key {
+			json.NewEncoder(w).Encode(emp)
+		}
 	}
+}
 
-	e.Logger.Fatal(e.Start(":" + httpPort))
+//post
+func createNewEmp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hi from create")
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	//fmt.Fprintf(w, "%+v", string(reqBody))
+	var emp Employee
+	json.Unmarshal(reqBody, &emp)
+	Employees = append(Employees, emp)
+	json.NewEncoder(w).Encode(emp)
+}
+
+//delete
+func deleteEmp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hi from delete")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	for index, emp := range Employees {
+		if emp.Id == id {
+
+			Employees = append(Employees[:index], Employees[index+1:]...)
+		}
+	}
+}
+
+//update PUT
+func updateEmp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("from update")
+	EmployeeID := mux.Vars(r)["id"]
+	var emp Employee
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(reqBody, &emp)
+
+	for i, looper := range Employees {
+		if looper.Id == EmployeeID {
+			looper.Id = emp.Id
+			looper.Name = emp.Name
+			Employees = append(Employees[:i], looper)
+			json.NewEncoder(w).Encode(looper)
+		}
+	}
+}
+
+// handlers
+func handleRequests() {
+	// http.HandleFunc("/employees", returnAllEmployees)
+	// http.HandleFunc("/", homePage)
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	//using  mux for better api usage
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/employees", returnAllEmployees)
+	myRouter.HandleFunc("/employee", createNewEmp).Methods("POST")
+	myRouter.HandleFunc("/employee/{id}", deleteEmp).Methods("DELETE")
+	myRouter.HandleFunc("/employee/{id}", updateEmp).Methods("PUT")
+	myRouter.HandleFunc("/employee/{id}", returnSingleEmp)
+	log.Fatal(http.ListenAndServe(":8089", myRouter))
+}
+
+func main() {
+	fmt.Println("Hi")
+
+	Employees = []Employee{
+		Employee{Id: "1032", Name: "Vishnu"},
+		Employee{Id: "1033", Name: "Abhi"},
+	}
+	handleRequests()
 }
